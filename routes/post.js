234b99968a30b5eddf418fc2969f4e2a4b539709
FindPecker <ayodeleayoola264@gmail.com>
2020-07-22 06:22:11 -0400

    first commit

--- routes/post.js
+++ routes/post.js
@@ -23,7 +23,7 @@ const router = express.Router();
 // Note: postController destructured
 // Note: validator destructured from validator
 
-router.get('/api/posts', getPosts);
+router.get('/posts', getPosts);
 router.post(
   '/post/new/:userId',
   requireSignin,
